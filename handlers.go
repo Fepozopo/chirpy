62e52763cba152eb0b116b48f076a689d6f2668c
Fepozopo <fepozopo.f39@gmail.com>
2024-12-15 14:37:17 -0800

    Organized the code from middleware.go

---
+++ handlers.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+)
+
+// handleMetrics returns the current hit counter for the file server in plain text as a number.
+func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	hits := cfg.fileserverHits.Load()
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits: " + strconv.Itoa(int(hits))))
+}
+
+// handleReset resets the file server hit counter to zero and responds with a plain text message
+// indicating that the hits have been reset.
+func (cfg *apiConfig) handleReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Hits reset to 0"))
+}
